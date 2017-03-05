a33dca4cdcb37aa9c94c97ec5902e00f1319162c
Maciej Matuszewski <maciek.matuszewski@gmail.com>
2017-03-05 14:26:28 +0100

    Complete project before expanding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,25 @@ import { LocationStrategy, PathLocationStrategy, HashLocationStrategy } from '@a
 import { AppComponent } from './app.component';
 import { MediaService } from './services/index';
 import { StartComponent } from './start/start.component';
+import { VideosComponent } from './videos/videos.component';
+import { VideoItemComponent } from './video-item/video-item.component';
+import { VideoProfileComponent } from './video-profile/video-profile.component';
+import { SavePipe } from './pipes/save.pipe';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { VideoAddComponent } from './video-add/video-add.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StartComponent,
+    VideosComponent,
+    VideoItemComponent,
+    VideoProfileComponent,
+    SavePipe,
+    LoginComponent,
+    RegisterComponent,
+    VideoAddComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,16 @@
 import { Routes, RouterModule } from '@angular/router';
-import { StartComponent } from './start/start.component'
+import { StartComponent } from './start/start.component';
+import { VideosComponent } from './videos/videos.component';
+import { VideoProfileComponent } from './video-profile/video-profile.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const appRoutes: Routes = [
-  { path: 'start', component: StartComponent },
+  { path: '', component: StartComponent },
+  { path: 'filmy/:category/:query', component: VideosComponent },
+  { path: 'profil/:ytid', component: VideoProfileComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'zarejestruj', component: RegisterComponent },
   { path: '**', redirectTo: 'start' }
 ]
 
